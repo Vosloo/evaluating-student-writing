910644092629f99595a829ee094433615e2edad5
Marek Szydlowski <marek.szydlow@gmail.com>
2022-06-25 16:58:42 +0200

    Added spacy large english model with embedding vectors

--- setup_repo.sh
+++ setup_repo.sh
@@ -5,6 +5,7 @@ tar xf data/dataset.tar.xz -C data
 
 # Pretrained pipeline models
 python -m spacy download en_core_web_trf
+python -m spacy download en_core_web_lg
 
 # Hooks
 pre-commit autoupdate
