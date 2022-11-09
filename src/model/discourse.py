bb3704e920a76fe8171e23b4e8bd78c306828bb7
Marek Szydlowski <marek.szydlow@gmail.com>
2022-11-10 00:17:21 +0100

    Improved pprint of discourse

--- src/model/discourse.py
+++ src/model/discourse.py
@@ -11,14 +11,15 @@ class Discourse:
         discourse_text: str,
         discourse_type: str,
     ):
-        self.discourse_id = discourse_id
-        self.discourse_start = discourse_start
-        self.discourse_end = discourse_end
-        self.discourse_text = discourse_text
-        self.discourse_type = discourse_type
+        self.id = discourse_id
+        self.ind_start = discourse_start
+        self.ind_end = discourse_end
+        self.text = discourse_text
+        self.type = discourse_type
 
     def __str__(self) -> str:
-        return self.discourse_text
+        header = f"--- {self.id} ({self.ind_start} -> {self.ind_end}) - {self.type} ---"
+        return f"{header}\n{self.text}\n{'-' * len(header)}"
 
     def __repr__(self) -> str:
         return repr(self.__str__())
