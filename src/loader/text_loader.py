1a9b88ec09b60e159ce97492afe89515860aa938
Marek Szydlowski <marek.szydlow@gmail.com>
2022-12-18 22:32:28 +0100

    Updated text_loader

--- src/loader/text_loader.py
+++ src/loader/text_loader.py
@@ -1,9 +1,10 @@
-from typing import Union, Iterator
+from typing import Iterator, Union
 
 import numpy as np
 import pandas as pd
+
 from definitions import ROOT_DIR
-from src.model import Discourse, Text, DiscourseType
+from src.model import DatasetType, Discourse, DiscourseType, Text
 from src.purifier import Purifier
 
 ID = "id"
@@ -14,17 +15,19 @@ DISCOURSE_END = "discourse_end"
 DISCOURSE_TEXT = "discourse_text"
 DISCOURSE_TYPE = "discourse_type"
 
+DATA_PATH = ROOT_DIR + "/data"
+DATASET_PATH = DATA_PATH + "/dataset"
+TRAIN_DIR = DATASET_PATH + "/train"
+
+ORIGINAL_DATASET = DATASET_PATH + "/train.csv"
+V1_NO_PREDICTIONSTRING_DATASET = DATA_PATH + "/train_v1_no_predictionstring.xz"
+V1_WITH_PREDICTIONSTRING_DATASET = DATA_PATH + "/train_v1_with_predictionstring.xz"
+
 
 class TextLoader:
-    DATASET_PATH = ROOT_DIR + "/data/dataset"
-    TRAIN_DIR = DATASET_PATH + "/train"
-    TEST_DIR = DATASET_PATH + "/test"
-    TRAIN_CSV = DATASET_PATH + "/train.csv"
-
-    def __init__(self) -> None:
-        self._df = pd.read_csv(self.TRAIN_CSV).astype(
-            {DISCOURSE_ID: np.int64, DISCOURSE_START: np.int64, DISCOURSE_END: np.int64}
-        )
+    def __init__(self, dataset_type: DatasetType = DatasetType.V1_WITH_PREDICTIONSTRING) -> None:
+        self._df = self._load_dataset(dataset_type)
+        self._dataset_type = dataset_type
 
         self.unique_ids = np.array(self._df[ID].unique())
         self.unique_ids_shuffled: Union[np.ndarray, None] = None
@@ -37,6 +40,20 @@ class TextLoader:
     def __len__(self) -> int:
         return len(self.unique_ids)
 
+    def _load_dataset(self, dataset_type: DatasetType) -> pd.DataFrame:
+        if dataset_type == DatasetType.ORIGINAL:
+            df = pd.read_csv(ORIGINAL_DATASET)
+        elif dataset_type == DatasetType.V1_NO_PREDICTIONSTRING:
+            df = pd.read_csv(V1_NO_PREDICTIONSTRING_DATASET, compression="xz")
+        elif dataset_type == DatasetType.V1_WITH_PREDICTIONSTRING:
+            df = pd.read_csv(V1_WITH_PREDICTIONSTRING_DATASET, compression="xz")
+        else:
+            raise ValueError(f"Unknown dataset type: {dataset_type}")
+
+        df = df.astype({DISCOURSE_ID: np.int64, DISCOURSE_START: np.int64, DISCOURSE_END: np.int64})
+
+        return df
+
     def _get_shuffled(self, seed: Union[int, None] = None) -> np.ndarray:
         if self.unique_ids_shuffled is None:
             if seed is not None:
@@ -47,30 +64,6 @@ class TextLoader:
             )
 
         return self.unique_ids_shuffled
-
-    # def _is_predictionstring_valid(
-    #     self, text_id: int, text: str, disc_start: int, disc_end: int, predictionstring: str
-    # ) -> tuple[bool, str]:
-    #     disc_words = text[disc_start:disc_end].split()
-    #     pred_idx = predictionstring.split()
-    #     pred_start, pred_end = int(pred_idx[0]), int(pred_idx[-1])
-
-    #     words = text.split()
-    #     if disc_words[0] in (".", ",", "!", "?"):
-    #         pred_end -= 1
-    #         del disc_words[0]
-
-    #     selected = words[pred_start : pred_end + 1]
-
-    #     disc_words[-1] = disc_words[-1].strip(".,!?")
-    #     selected[-1] = selected[-1].strip(".,!?")
-
-    #     if disc_words != selected:
-    #         print("id:", text_id)
-    #         print(f"{disc_words} !=\n{selected}", end="\n\n")
-    #         return False, ""
-
-    #     return True, " ".join(range(pred_start, pred_end + 1))
 
     @property
     def df(self) -> pd.DataFrame:
@@ -93,6 +86,10 @@ class TextLoader:
         :param limit: The maximum number of texts to iterate over.
         :param shuffle: Whether to shuffle the texts before iterating.
         :param seed: The seed to use for shuffling (if shuffle is True).
+        :param purify_text: Whether to purify the text.
+        :param purify_discourses:
+            Whether to purify the discourses. (does not apply for purified datasets)
+        :param verbose: Whether to print the progress.
 
         :return: A generator that yields the text object.
         """
@@ -114,40 +111,52 @@ class TextLoader:
     def load_text_with_id(
         self, text_id: str, purify_discourses: bool = False, purify_text: bool = False
     ) -> Text:
-        with open(self.TRAIN_DIR + "/" + text_id + ".txt", "r", encoding="utf-8") as f:
+        """
+        Load a text object from the dataset.
+
+        :param text_id: The id of the text to load.
+        :param purify_discourses:
+            Whether to purify the discourses. (does not apply for purified datasets)
+        :param purify_text: Whether to purify the text.
+
+        :return: The text object.
+        """
+        with open(TRAIN_DIR + "/" + text_id + ".txt", "r", encoding="utf-8") as f:
             text = f.read()
 
         if purify_text:
             text = self.purifier.purify(text)
 
         discourses_df: pd.DataFrame = self._df.loc[self._df[ID] == text_id].reset_index(drop=True)
-        if purify_discourses:
+        if purify_discourses and self._dataset_type == DatasetType.ORIGINAL:
             discourses_df.loc[:, DISCOURSE_TEXT] = discourses_df[DISCOURSE_TEXT].apply(
                 self.purifier.purify
             )
 
         discourses = []
         for row in discourses_df.itertuples():
-            # (is_predictionstring_valid, predictionstring) = self._is_predictionstring_valid(
-            #     text_id, text, row.discourse_start, row.discourse_end, row.predictionstring  # type: ignore[attr-defined] # noqa: E501
-            # )
-
             discourse = Discourse(
                 row.discourse_id,  # type: ignore[attr-defined]
                 row.discourse_start,  # type: ignore[attr-defined]
                 row.discourse_end,  # type: ignore[attr-defined]
                 row.discourse_text,  # type: ignore[attr-defined]
                 DiscourseType(row.discourse_type),  # type: ignore[attr-defined]
-                # self._is_predictionstring_valid(
-                #     text_id, text, row.discourse_start, row.discourse_end, row.predictionstring  # type: ignore[attr-defined] # noqa: E501
-                # ),
                 row.predictionstring,  # type: ignore[attr-defined]
             )
             discourses.append(discourse)
 
         return Text(text_id, text, discourses)
 
-    def load_random_text(self, purify_discourses: bool = False) -> Text:
+    def load_random_text(self, purify_discourses: bool = False, purify_text: bool = False) -> Text:
+        """
+        Load a random text object from the dataset.
+
+        :param purify_discourses:
+            Whether to purify the discourses. (does not apply for purified datasets)
+        :param purify_text: Whether to purify the text.
+
+        :return: The text object.
+        """
         text_id = np.random.choice(self.unique_ids)
 
-        return self.load_text_with_id(text_id, purify_discourses)
+        return self.load_text_with_id(text_id, purify_discourses, purify_text)
