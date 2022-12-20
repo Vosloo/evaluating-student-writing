aff939dd57f37c07a04f2488e3ab4085d21c6109
Marek Szydlowski <marek.szydlow@gmail.com>
2022-12-20 20:59:03 +0100

    Added train script; Updated train notebook

---
+++ notebooks/NER/train.py
@@ -0,0 +1,112 @@
+import sys
+from pathlib import Path
+
+module_path = Path.cwd().parent.parent
+if module_path not in sys.path:
+    sys.path.append(str(module_path))
+
+import torch  # noqa: E402
+import wandb  # noqa: E402
+from flair.data import Corpus, Sentence  # noqa: E402
+from flair.datasets import ColumnCorpus  # noqa: E402
+from flair.embeddings import (  # noqa: E402 F401
+    FlairEmbeddings,
+    StackedEmbeddings,
+    TransformerWordEmbeddings,
+    WordEmbeddings,
+)
+from flair.models import SequenceTagger  # noqa: E402
+from flair.trainers import ModelTrainer  # noqa: E402
+
+from src.loader import TextLoader  # noqa: E402
+from src.model import DatasetType  # noqa: E402
+
+wandb.init(project="discourse-ner-test", entity="evaluating-student-writing")
+wandb.config = {
+    "learning_rate": 2,
+    "mini_batch_size": 128,
+    "max_epochs": 1,
+}
+
+loader = TextLoader(DatasetType.V1_WITH_PREDICTIONSTRING)
+
+columns = {0: "text", 1: "ner"}
+corpus: Corpus = ColumnCorpus(
+    "data/",
+    columns,
+    train_file="NER_train.txt",
+    dev_file="NER_dev.txt",
+    test_file="NER_test.txt",
+    column_delimiter=" ",
+    document_separator_token="<DOC>",
+    in_memory=True,
+)
+corpus.filter_empty_sentences()
+
+
+label_dict = corpus.make_label_dictionary(label_type="ner")
+
+embedding_types = [
+    # GloVe embeddings>
+    WordEmbeddings("glove"),
+    # contextual string embeddings, forward
+    # FlairEmbeddings('news-forward'),
+    # contextual string embeddings, backward
+    # FlairEmbeddings('news-backward'),
+]
+
+embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
+
+tagger = SequenceTagger(
+    hidden_size=256,
+    embeddings=embeddings,
+    tag_dictionary=label_dict,
+    tag_type="ner",
+    use_crf=True,
+)
+
+wandb.watch(tagger)
+
+
+torch.cuda.empty_cache()
+
+trainer = ModelTrainer(tagger, corpus)
+
+checkpoint = "models/checkpoint.pt"
+if Path(checkpoint).exists():
+    tagger = SequenceTagger.load(checkpoint)
+    trainer.resume(
+        tagger,
+        base_path="models/",
+        max_epochs=30,
+    )
+else:
+    trainer.train(
+        "models/",
+        learning_rate=2,
+        mini_batch_size=128,
+        max_epochs=1,
+        patience=4,
+        anneal_factor=0.5,
+        num_workers=8,
+        monitor_train=True,
+        write_weights=True,
+        create_file_logs=True,
+        create_loss_file=True,
+        checkpoint=True,
+        embeddings_storage_mode="gpu",
+        train_with_dev=False,
+    )
+
+model = SequenceTagger.load("models/final-model.pt")
+
+text = loader.load_random_text()
+
+sent = Sentence(text.text)
+model.predict(sent)
+
+for disc in text.discourses:
+    print(disc)
+
+print()
+print(sent.to_tagged_string())
